2df281c17fd9a21955e16a6d23d88813845b56bb
11carmel11 <11carmrl11@gmail.com>
2021-10-23 18:10:36 +0300

    adjusting to back-end

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ const userInput = document.getElementById("search-query");
 const nameElm = document.getElementById("pokéName");
 const weightElm = document.getElementById("weight");
 const heightElm = document.getElementById("height");
-const photo = document.getElementById("photo");
+const photo = document.getElementById("front-photo");
 const resultSection = document.getElementById("result-section");
 const typesElm = document.getElementById("types");
 const brotherSection = document.getElementById("brother");
-
+const mineSection = document.getElementById("mine");
+const catchButton = document.getElementById("catch");
+const releaseButton = document.getElementById("release");
+const server = "http://localhost:8080/";
+let backPhoto = "";
+let frontPhoto = "";
+let pokemon;
+let username;
+getUsername();
 //alert fail
 const fail = () => {
   alert("Oops, something went wrong...");
@@ -22,18 +30,23 @@ async function searchHandler() {
     }
     if (!userInput.value) fail();
     else {
-      const jsonAns = await fetch(
-        `https://pokeapi.co/api/v2/pokemon/${userInput.value}`
-      );
-      const ans = await jsonAns.json();
-      nameElm.innerHTML = `<b>name: </b>${ans.name}`;
-      weightElm.innerHTML = `<b>weight: </b>${ans.weight}`;
-      heightElm.innerHTML = `<b>height: </b>${ans.height}`;
-      buildTypesRow(ans);
-      photo.setAttribute(
-        "src",
-        `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${ans.id}.png`
-      );
+      const parsed = parseInt(userInput.value);
+      let jsonAns;
+      if (parsed === "NaN") {
+        jsonAns = await axios.get(`${server}pokemon/query?query=${userInput}`);
+      } else {
+        jsonAns = await axios.get(`${server}pokemon/get/${userInput.value}`);
+      }
+      pokemon = jsonAns.data;
+      catchButton.innerText = "catch";
+      releaseButton.innerText = "release";
+      nameElm.innerHTML = `<b>name: </b>${pokemon.name}`;
+      weightElm.innerHTML = `<b>weight: </b>${pokemon.weight}`;
+      heightElm.innerHTML = `<b>height: </b>${pokemon.height}`;
+      buildTypesRow(pokemon);
+      frontPhoto = pokemon.front_pic;
+      backPhoto = pokemon.back_pic;
+      photo.setAttribute("src", frontPhoto);
       resultSection.removeAttribute("hidden");
     }
   } catch (error) {
@@ -44,22 +57,13 @@ async function searchHandler() {
 // shows the back of the pokemon
 function flipPhotoToBack() {
   if (!photo.src) return;
-  const pokéId = getIdFromSrc();
-  photo.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${pokéId}.png`;
-}
-
-// takes url and extract the id
-function getIdFromSrc() {
-  const arr = photo.src.split("/");
-  const lastPartOfSrc = arr[arr.length - 1].split(".");
-  return lastPartOfSrc[0];
+  photo.src = backPhoto;
 }
 
 // shows the front of the pokemon
 function flipPhotoToFront() {
   if (!photo.src) return;
-  const pokéId = getIdFromSrc();
-  photo.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokéId}.png`;
+  photo.src = frontPhoto;
 }
 
 // extracts the types of the pokemon and append them in place
@@ -67,7 +71,7 @@ function buildTypesRow(obj) {
   typesElm.innerHTML = "";
   for (let type of obj.types) {
     typesElm.append(
-      createElement("button", type.type.name, {
+      createElement("button", type, {
         onclick: "viewBrothers(event)",
         class: "point",
       })
@@ -87,6 +91,7 @@ function createElement(type, str, atrs = {}) {
 // appends the brothers of the pokemon
 async function viewBrothers(event) {
   try {
+    brotherSection.innerHTML = "";
     const text = event.target.innerHTML;
     const jsonResponse = await fetch(`https://pokeapi.co/api/v2/type/${text}`);
     const response = await jsonResponse.json();
@@ -108,8 +113,64 @@ async function viewBrothers(event) {
 // present the selected brother as main pokemon
 function presentNewPokemon(event) {
   userInput.value = event.target.innerHTML;
+  mineSection.innerHTML = "";
   brotherSection.innerHTML = "";
   searchHandler().finally(() => {
     userInput.value = "";
   });
+}
+function errorShower(error) {
+  brotherSection.innerHTML = error.response.data.error;
+  brotherSection.classList.add("error");
+  setTimeout(() => {
+    brotherSection.innerHTML = "";
+    brotherSection.classList.remove("error");
+  }, 1500);
+}
+async function getUsername() {
+  username = prompt("please enter username");
+  const ans = await axios.post(`${server}info/`, JSON.stringify({ username }));
+}
+
+async function catchPoke() {
+  try {
+    const headers = { username: username };
+    const ans = await axios.put(
+      `${server}pokemon/catch/${pokemon.id}`,
+      { pokemon },
+      { headers }
+    );
+    catchButton.innerText = ans.data;
+  } catch (error) {
+    errorShower(error);
+  }
+}
+
+async function releasePoke() {
+  try {
+    const headers = { username: username };
+    const ans = await axios.delete(`${server}pokemon/release/${pokemon.id}`, {
+      headers,
+    });
+    catchButton.innerText = "catch";
+  } catch (error) {
+    errorShower(error);
+  }
+}
+
+async function showList() {
+  try {
+    mineSection.innerHTML = "";
+    const headers = { username: username };
+    const ans = await axios.get(`${server}pokemon/`, { headers });
+    for (let poke of ans.data) {
+      mineSection.append(
+        createElement("li", poke.name, {
+          onclick: "presentNewPokemon(event)",
+          class: "point mine",
+        })
+      );
+      mineSection.append(createElement("br"));
+    }
+  } catch (error) {}
 }
